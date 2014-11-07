1cbc533038cbd2d8e4e41df467c79b07fd07ea9f
jolson7168 <technology@nododos.com>
2014-11-07 16:08:46 -0600

    changed test payload

--- src/listener2.R
+++ src/listener2.R
@@ -11,39 +11,7 @@ app <- list(
 			headers = list(
 				'Content-Type' = 'text/html'
 			),
-			body = paste(
-				sep = "\r\n",
-				"<!DOCTYPE html>",
-				"<html>",
-				"<head>",
-				'<style type="text/css">',
-				'body { font-family: Helvetica; }',
-				'pre { margin: 0 }',
-				'</style>',
-				"<script>",
-				sprintf("var ws = new WebSocket(%s);", wsUrl),
-				"ws.onmessage = function(msg) {",
-				' var msgDiv = document.createElement("pre");',
-				' msgDiv.innerHTML = msg.data.replace(/&/g, "&amp;").replace(/\\</g, "&lt;");',
-				' document.getElementById("output").appendChild(msgDiv);',
-				"}",
-				"function sendInput() {",
-				" var input = document.getElementById('input');",
-				" ws.send(input.value);",
-				" input.value = '';",
-				"}",
-				"</script>",
-				"</head>",
-				"<body>",
-				'<h3>Send Message</h3>',
-				'<form action="" onsubmit="sendInput(); return false">',
-				'<input type="text" id="input"/>',
-				'<h3>Received</h3>',
-				'<div id="output"/>',
-				'</form>',
-				"</body>",
-				"</html>"
-				)
+			body = "hello world!"
 		)
 	},
 
